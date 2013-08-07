1ca79c94e50affa906272d3d2746edc358c9380b
flyers2 <Nizukan@yahoo.com>
2013-08-06 21:41:31 -0400

    commiting array

--- sort_array.php
+++ sort_array.php
@@ -2,9 +2,11 @@
 
 	$products = array("knife",' clock',' pens',' beer');
 	sort($products);
-
+	// this does not work I don't know why!$products[]= array("apple", 'tshirt')'
+	$products[] = 'cars';  //will not get sorted 
+	echo "<pre>";
 	print_r( $products) ;
-
+	echo "</pre>";
 
 	
 	
